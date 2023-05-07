48f61339be5b718a22c7c8468e94d47d6e05caa3
Charlieo123 <99101639+Charlieo123@users.noreply.github.com>
2023-05-07 16:04:34 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+function displayText4() {
+    var text4 = document.getElementById("textField4");
+    var text3 = document.getElementById("textField3");
+    var text2 = document.getElementById("textField2");
+    var text1 = document.getElementById("textField1");
+    text4.style.display = "block";
+    text3.style.display = "none";
+    text2.style.display = "none";
+    text1.style.display = "none";
+}
+
+function displayText3() {
+    var text4 = document.getElementById("textField4");
+    var text3 = document.getElementById("textField3");
+    var text2 = document.getElementById("textField2");
+    var text1 = document.getElementById("textField1");
+    text3.style.display = "block";
+    text4.style.display = "none";
+    text2.style.display = "none";
+    text1.style.display = "none";
+}
+
+function displayText2() {
+    var text4 = document.getElementById("textField4");
+    var text3 = document.getElementById("textField3");
+    var text2 = document.getElementById("textField2");
+    var text1 = document.getElementById("textField1");
+    text2.style.display = "block";
+    text3.style.display = "none";
+    text4.style.display = "none";
+    text1.style.display = "none";
+}
+
+function displayText1() {
+    var text4 = document.getElementById("textField4");
+    var text3 = document.getElementById("textField3");
+    var text2 = document.getElementById("textField2");
+    var text1 = document.getElementById("textField1");
+    text1.style.display = "block";
+    text3.style.display = "none";
+    text4.style.display = "none";
+    text2.style.display = "none";
+}
+
